9edf02ecc86862475c4d2880965841eea6e7ad3c
EderMoritz <moritz.eder@liwest.at>
2022-11-13 11:28:35 +0100

    create packages && translate classes to english

---
+++ code/backend/src/main/java/at/htl/entities/Customer.java
@@ -0,0 +1,14 @@
+package at.htl.entities;
+
+public class Customer {
+
+    int customerID;
+    String firstname;
+    String lastname;
+    Invoice[] invoice;
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ code/backend/src/main/java/at/htl/entities/InvoicePosition.java
@@ -0,0 +1,11 @@
+package at.htl.entities;
+
+public class InvoicePosition {
+
+    int productID;
+    int quantity;
+
+    public static void main(String[] args) {
+
+    }
+}
